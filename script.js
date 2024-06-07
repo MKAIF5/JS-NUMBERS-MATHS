72df9ee24186f8d4ebcccc00787b7766b47f9756
mohammed kaif <mkaif555123@gmail.com>
2024-06-07 15:36:29 +0500

    to fixed and to precien topic

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let num = new Number(100)
+const num = new Number(100)
 console.log(num.toString());
+console.log(num.toFixed(2));
+
+const numPractice = 24.4647;
+
+console.log(numPractice.toFixed(5));
