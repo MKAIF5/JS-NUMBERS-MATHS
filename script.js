a1b46d86f752989011d246bd7ab42b9c9be1fd45
mohammed kaif <mkaif555123@gmail.com>
2024-06-07 20:45:34 +0500

    maths topics practices

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const num = new Number(100)
-console.log(num.toString());
-console.log(num.toFixed(2));
+// const num = new Number(100)
+// console.log(num.toString());
+// console.log(num.toFixed(2));
 
-const numPractice = 24.4647;
+// const numPractice = 24.4647;
 
-console.log(numPractice.toFixed(5));
+// console.log(numPractice.toFixed(5));
 
-const pointChange = Math.floor(numPractice)
+// const pointChange = Math.floor(numPractice)
 
-const numRound = 32.7
+// const numRound = 32.7
 
-console.log(Math.round(numRound));
+// console.log(Math.round(numRound));
+
+// console.log(Math);
+
+// console.log(Math.abs(-+3));
+
+console.log(Math.round(4.49999));
